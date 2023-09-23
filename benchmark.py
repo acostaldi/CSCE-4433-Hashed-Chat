d7c369c285c76bbf79795eab35b9c21623bb18b4
LTShrub <amadeom.costaldi@gmail.com>
2023-09-23 16:52:26 -0500

    corrected benchmark tests

--- benchmark.py
+++ benchmark.py
@@ -6,110 +6,106 @@ from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import unpad, pad
 
-def benchmark_aes_encryption(test_message):
-    #Generate AES keys
-    aes_key128 = get_random_bytes(16)
-    aes_key192 = get_random_bytes(24)
-    aes_key256 = get_random_bytes(32)
+def benchmark_aes(test_message):
+    key_sizes = [128, 192, 256]
+    average_encryption_times = []
+    average_decryption_times = []
 
-    intializationVector = get_random_bytes(16)
-    
-    #Decrypt and measure time
-    start_time = time.time()
+    for key_size in key_sizes:
+        # Generate AES keys
+        aes_key = get_random_bytes(key_size // 8)
+        intializationVector = get_random_bytes(16)
 
-    aes_cipher128 = AES.new(aes_key128, AES.MODE_CBC, intializationVector)
-    aes_cipher192 = AES.new(aes_key192, AES.MODE_CBC, intializationVector)
-    aes_cipher256 = AES.new(aes_key256, AES.MODE_CBC, intializationVector)
+        # Encryption
+        encryption_times = []
 
-    plaintext = test_message[:-(len(test_message) % 16)].encode('utf-8')
-    plaintext = pad(test_message.encode('utf-8'), AES.block_size)
+        for _ in range(100):
+            start_time = time.time()
 
-    ciphertext128 = aes_cipher128.encrypt(plaintext)
-    ciphertext192 = aes_cipher192.encrypt(plaintext)
-    ciphertext256 = aes_cipher256.encrypt(plaintext)
-    
-    print("Encrypted 128-bit:", ciphertext128.hex())
-    print("Encrypted 192-bit:", ciphertext192.hex())
-    print("Encrypted 256-bit:", ciphertext256.hex())
-    
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print("AES Average Encryption time = {:.6f} seconds".format(elapsed_time / 3))
+            aes_cipher = AES.new(aes_key, AES.MODE_CBC, intializationVector)
 
-    #Decrypt and measure time
-    start_time = time.time()
+            plaintext = test_message[:-(len(test_message) % 16)].encode('utf-8')
+            plaintext = pad(test_message.encode('utf-8'), AES.block_size)
 
-    decipher128 = AES.new(aes_key128, AES.MODE_CBC, intializationVector)
-    decipher192 = AES.new(aes_key192, AES.MODE_CBC, intializationVector)
-    decipher256 = AES.new(aes_key256, AES.MODE_CBC, intializationVector)
+            ciphertext = aes_cipher.encrypt(plaintext)
 
-    decrypted128 = decipher128.decrypt(ciphertext128)
-    decrypted192 = decipher192.decrypt(ciphertext192)
-    decrypted256 = decipher256.decrypt(ciphertext256)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            encryption_times.append(elapsed_time)
 
-    #Unpad the decrypted plaintext
-    plaintext128 = unpad(decrypted128, AES.block_size).decode('utf-8')
-    plaintext192 = unpad(decrypted192, AES.block_size).decode('utf-8')
-    plaintext256 = unpad(decrypted256, AES.block_size).decode('utf-8')
+        average_encryption_time = sum(encryption_times) / len(encryption_times)
+        average_encryption_times.append(average_encryption_time)
 
-    end_time = time.time()
+        # Decryption
+        decryption_times = []
 
-    #Print decrypted values
-    print("Decrypted 128-bit:", plaintext128)
-    print("Decrypted 192-bit:", plaintext192)
-    print("Decrypted 256-bit:", plaintext256)
+        for _ in range(100):
+            start_time = time.time()
 
-    #Print the time taken for decryption
-    elapsed_time = end_time - start_time
-    print("AES Average Encryption time = {:.6f} seconds".format(elapsed_time / 3))
+            decipher = AES.new(aes_key, AES.MODE_CBC, intializationVector)
 
-def benchmark_rsa_encryption(test_message):
-    #Generate RSA keys
-    rsa_key1024 = RSA.generate(1024)
-    rsa_key2048 = RSA.generate(2048)
-    rsa_key4096 = RSA.generate(4096)
+            decrypted = decipher.decrypt(ciphertext)
 
-    #Encrypt and measure time
-    start_time = time.time()
+            # Unpad the decrypted plaintext
+            plaintext = unpad(decrypted, AES.block_size).decode('utf-8')
 
-    cipher1024 = PKCS1_OAEP.new(rsa_key1024)
-    cipher2048 = PKCS1_OAEP.new(rsa_key2048)
-    cipher4096 = PKCS1_OAEP.new(rsa_key4096)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            decryption_times.append(elapsed_time)
 
-    ciphertext1024 = cipher1024.encrypt(test_message.encode('utf-8'))
-    ciphertext2048 = cipher2048.encrypt(test_message.encode('utf-8'))
-    ciphertext4096 = cipher4096.encrypt(test_message.encode('utf-8'))
+        average_decryption_time = sum(decryption_times) / len(decryption_times)
+        average_decryption_times.append(average_decryption_time)
 
-    print("Encrypted 1024-bit:", ciphertext1024.hex())
-    print("Encrypted 2048-bit:", ciphertext2048.hex())
-    print("Encrypted 4096-bit:", ciphertext4096.hex())
+        print(f"AES {key_size}-bit Average Encryption Time = {average_encryption_time:.6f} seconds")
+        print(f"AES {key_size}-bit Average Decryption Time = {average_decryption_time:.6f} seconds\n")
+        
+def benchmark_rsa(test_message):
+    key_sizes = [1024, 2048, 4096]
+    average_encryption_times = []
+    average_decryption_times = []
 
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print("RSA Average Encryption time = {:.6f} seconds".format(elapsed_time / 3))
+    for key_size in key_sizes:
+        # Generate RSA keys
+        rsa_key = RSA.generate(key_size)
 
-    #Decrypt and measure time
-    start_time = time.time()
+        # Encryption
+        encryption_times = []
 
-    plaintext1024 = cipher1024.decrypt(ciphertext1024).decode('utf-8')
-    plaintext2048 = cipher2048.decrypt(ciphertext2048).decode('utf-8')
-    plaintext4096 = cipher4096.decrypt(ciphertext4096).decode('utf-8')
+        for _ in range(100):
+            start_time = time.time()
 
-    print("Decrypted 1024-bit:", plaintext1024)
-    print("Decrypted 2048-bit:", plaintext2048)
-    print("Decrypted 4096-bit:", plaintext4096)
+            cipher = PKCS1_OAEP.new(rsa_key)
+            ciphertext = cipher.encrypt(test_message.encode('utf-8'))
 
-    end_time = time.time()
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            encryption_times.append(elapsed_time)
 
-    #Print the time taken for decryption
-    elapsed_time = end_time - start_time
-    print("RSA Average Decryption time = {:.6f} seconds".format(elapsed_time / 3))
+        average_encryption_time = sum(encryption_times) / len(encryption_times)
+        average_encryption_times.append(average_encryption_time)
 
+        # Decryption
+        decryption_times = []
+
+        for _ in range(100):
+            start_time = time.time()
+
+            plaintext = cipher.decrypt(ciphertext).decode('utf-8')
+
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            decryption_times.append(elapsed_time)
+
+        average_decryption_time = sum(decryption_times) / len(decryption_times)
+        average_decryption_times.append(average_decryption_time)
+
+        print(f"RSA {key_size}-bit Average Encryption Time = {average_encryption_time:.6f} seconds")
+        print(f"RSA {key_size}-bit Average Decryption Time = {average_decryption_time:.6f} seconds\n")
     
 def main():
     messageIn = input("Enter test value: ")
-    benchmark_aes_encryption(messageIn)
-    benchmark_rsa_encryption(messageIn)
+    benchmark_aes(messageIn)
+    benchmark_rsa(messageIn)
         
 if __name__ == "__main__":
         main()
